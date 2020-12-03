371f25dce97a18913b6a1310711a657d5a05b47b
cmeehan340 <c.meehan340@gmail.com>
2020-12-03 09:06:15 -0500

    Version 2.0 of the web app, getting data and printint to console now

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,6 +1,6 @@
 import os
 import io
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
 from google.cloud import translate
 from google.cloud import vision
@@ -15,6 +15,29 @@ def index():
 
 @app.route('/predict', methods=["GET", "POST"])
 def predict():
+    if request.method == "POST":
+        req = request.form
+        print(req)
+        gender = req['gender']
+        age = req['age']
+        education = req['edu']
+        smoke = req['smoke']
+        num_smoke = req['num_smoke']
+        if smoke == "no":
+            num_smoke = 0
+        bpm = req['bpm']
+        stroke = req['stroke']
+        hyp = req['hyp']
+        diab = req['diab']
+        totChol = req['TotChol']
+        sysBP = req['sysBP']
+        diaBP = req['diaBP']
+        bmi = req['bmi']
+        hr = req['hr']
+        gluc = req['gluc']
+        print(gender, age, education, smoke, num_smoke, bpm, stroke, hyp, diab, totChol, sysBP, diaBP, bmi, hr, gluc)
+        return redirect(request.url)
+
     return render_template("predict.html")
         
 if __name__ == '__main__':
