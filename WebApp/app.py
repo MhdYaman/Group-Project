63db38583a006401f443c9893c8b609a75c6e9c0
cmeehan340 <c.meehan340@gmail.com>
2020-12-03 12:42:56 -0500

    further fixes for web app

--- WebApp/app.py
+++ WebApp/app.py
@@ -8,34 +8,37 @@ from google.cloud import vision
 # Initialise Flask
 app = Flask(__name__)
 
+def getData(request):
+    req = request.form
+    gender = req['gender']
+    age = req['age']
+    education = req['edu']
+    smoke = req['smoke']
+    num_smoke = req['num_smoke']
+    if smoke == "no":
+        num_smoke = 0
+    bpm = req['bpm']
+    stroke = req['stroke']
+    hyp = req['hyp']
+    diab = req['diab']
+    totChol = req['TotChol']
+    sysBP = req['sysBP']
+    diaBP = req['diaBP']
+    bmi = req['bmi']
+    hr = req['hr']
+    gluc = req['gluc']
+    data = [int(gender), int(age), int(education), int(smoke), int(num_smoke), int(bpm), int(stroke), int(hyp), int(diab), float(totChol), float(sysBP), float(diaBP), float(bmi), float(hr), float(gluc)]
+    return(data)
+
 @app.route('/')
 def index():
-    
     return render_template('index.html')
 
 @app.route('/predict', methods=["GET", "POST"])
 def predict():
     if request.method == "POST":
-        req = request.form
-        print(req)
-        gender = req['gender']
-        age = req['age']
-        education = req['edu']
-        smoke = req['smoke']
-        num_smoke = req['num_smoke']
-        if smoke == "no":
-            num_smoke = 0
-        bpm = req['bpm']
-        stroke = req['stroke']
-        hyp = req['hyp']
-        diab = req['diab']
-        totChol = req['TotChol']
-        sysBP = req['sysBP']
-        diaBP = req['diaBP']
-        bmi = req['bmi']
-        hr = req['hr']
-        gluc = req['gluc']
-        print(gender, age, education, smoke, num_smoke, bpm, stroke, hyp, diab, totChol, sysBP, diaBP, bmi, hr, gluc)
+        data = getData(request)
+        print(data)
         return redirect(request.url)
 
     return render_template("predict.html")
