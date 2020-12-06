be84927e759c1c94f26525395b2104a6da038cad
cmeehan340 <c.meehan340@gmail.com>
2020-12-06 12:45:21 -0500

    Deployed Model and got web app to call out and make predictions

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,6 +1,9 @@
 import os
 import io
+import numpy as np
 from flask import Flask, render_template, request, redirect
+import googleapiclient.discovery
+from google.api_core.client_options import ClientOptions
 
 
 # Initialise Flask
@@ -10,23 +13,54 @@ def getData(request):
     req = request.form
     gender = req['gender']
     age = req['age']
-    education = req['edu']
     smoke = req['smoke']
     num_smoke = req['num_smoke']
-    if smoke == "no":
+    if smoke == "0":
         num_smoke = 0
-    bpm = req['bpm']
     stroke = req['stroke']
-    hyp = req['hyp']
-    diab = req['diab']
-    totChol = req['TotChol']
     sysBP = req['sysBP']
-    diaBP = req['diaBP']
-    bmi = req['bmi']
-    hr = req['hr']
     gluc = req['gluc']
-    data = [int(gender), int(age), int(education), int(smoke), int(num_smoke), int(bpm), int(stroke), int(hyp), int(diab), float(totChol), float(sysBP), float(diaBP), float(bmi), float(hr), float(gluc)]
+    data = [int(gender), int(age), int(num_smoke), int(stroke), float(sysBP), float(gluc)]
     return(data)
+
+def predict_json(project, region, model, instances, version=None):
+    """Send json data to a deployed model for prediction.
+
+    Args:
+        project (str): project where the Cloud ML Engine Model is deployed.
+        region (str): regional endpoint to use; set to None for ml.googleapis.com
+        model (str): model name.
+        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
+            your deployed model expects as inputs. Values should be datatypes
+            convertible to Tensors, or (potentially nested) lists of datatypes
+            convertible to tensors.
+        version: str, version of the model to target.
+    Returns:
+        Mapping[str: any]: dictionary of prediction results defined by the
+            model.
+    """
+    # Create the ML Engine service object.
+    # To authenticate set the environment variable
+    # GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
+    prefix = "{}-ml".format(region) if region else "ml"
+    api_endpoint = "https://{}.googleapis.com".format(prefix)
+    client_options = ClientOptions(api_endpoint=api_endpoint)
+    service = googleapiclient.discovery.build(
+        'ml', 'v1', client_options=client_options)
+    name = 'projects/{}/models/{}'.format(project, model)
+
+    if version is not None:
+        name += '/versions/{}'.format(version)
+
+    response = service.projects().predict(
+        name=name,
+        body={'instances': instances}
+    ).execute()
+
+    if 'error' in response:
+        raise RuntimeError(response['error'])
+
+    return response['predictions']
 
 @app.route('/')
 def index():
@@ -37,6 +71,8 @@ def predict():
     if request.method == "POST":
         data = getData(request)
         print(data)
+        response = predict_json('steam-bee-290914','us-east1','Heart_Disease',[data])
+        print(response)
         return redirect(request.url)
 
     return render_template("predict.html")
