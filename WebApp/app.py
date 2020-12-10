41b2edafe1f050f3000ed3afb42e3ae8332eb697
cmeehan340 <c.meehan340@gmail.com>
2020-12-10 17:02:16 -0500

    Added cloud function and working webapp to callout

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,9 +1,10 @@
 import os
 import io
 import numpy as np
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, escape
 import googleapiclient.discovery
 from google.api_core.client_options import ClientOptions
+import requests
 
 
 # Initialise Flask
@@ -20,44 +21,12 @@ def getData(request):
     data = [int(gender), int(age), int(num_smoke), int(stroke), float(sysBP), float(gluc)]
     return(data)
 
-def predict_json(project, region, model, instances, version=None):
-    """Send json data to a deployed model for prediction.
-
-    Args:
-        project (str): project where the Cloud ML Engine Model is deployed.
-        region (str): regional endpoint to use; set to None for ml.googleapis.com
-        model (str): model name.
-        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
-            your deployed model expects as inputs. Values should be datatypes
-            convertible to Tensors, or (potentially nested) lists of datatypes
-            convertible to tensors.
-        version: str, version of the model to target.
-    Returns:
-        Mapping[str: any]: dictionary of prediction results defined by the
-            model.
-    """
-    # Create the ML Engine service object.
-    # To authenticate set the environment variable
-    GOOGLE_APPLICATION_CREDENTIALS="key.json"
-    prefix = "{}-ml".format(region) if region else "ml"
-    api_endpoint = "https://{}.googleapis.com".format(prefix)
-    client_options = ClientOptions(api_endpoint=api_endpoint)
-    service = googleapiclient.discovery.build(
-        'ml', 'v1', client_options=client_options)
-    name = 'projects/{}/models/{}'.format(project, model)
-
-    if version is not None:
-        name += '/versions/{}'.format(version)
-
-    response = service.projects().predict(
-        name=name,
-        body={'instances': instances}
-    ).execute()
-
-    if 'error' in response:
-        raise RuntimeError(response['error'])
-
-    return response['predictions']
+def callApi(data):
+    send = {'instances':[]}
+    send['instances'] = data
+    url = "https://us-east1-heartdisease-297903.cloudfunctions.net/heart-disease"
+    reply = requests.post(url, json=send)
+    return reply.text
 
 @app.route('/')
 def index():
@@ -68,14 +37,12 @@ def predict():
     response = ""
     if request.method == "POST":
         data = getData(request)
-        print(data)
-        reply = predict_json('heartdisease-297903','us-east1','Heart_Disease_New',[data])
-        print(reply)
-        reply = reply[0]
-        if reply == 0:
-            response = "It is unlikely that you will have heart disease in 10 years"
-        elif reply == 1:
-            response = "It is likely that you will have heart disease in 10 years"
+        get_reply = callApi(data)
+        reply = get_reply[16]
+        if reply == '1':
+            response = "It is likely that you will develop heart disease in 10 years"
+        else:
+            response = "It is unlikely that you will develop heart disease in 10 years"
     return render_template("predict.html", result=response)
         
 if __name__ == '__main__':
